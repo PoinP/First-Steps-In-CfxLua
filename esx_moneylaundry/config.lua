d3a836ac69c7cc8fd84c939877413a48dcb055fc
PoinP <58052033+PoinP@users.noreply.github.com>
2021-02-28 00:11:02 +0200

    Made the laundry time more reasonable.

--- esx_moneylaundry/config.lua
+++ esx_moneylaundry/config.lua
@@ -4,7 +4,7 @@ Config.Locale = "bg"
 
 Config.Location = vector3(-2285.963, 353.8863, 174.6018)
 
-Config.TimeToLaunder = 3 --In Seconds
+Config.TimeToLaunder = 30 --In Seconds
 
 Config.AmountPerLaundry = 1000
 
