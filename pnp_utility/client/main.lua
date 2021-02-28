16ac8779d31b34a2010fa315b34579e346486eee
PoinP <58052033+PoinP@users.noreply.github.com>
2021-03-01 01:27:31 +0200

    A collection of scripts.

---
+++ pnp_utility/client/main.lua
@@ -0,0 +1,30 @@
+--Enable PvP
+AddEventHandler(
+    "playerSpawned",
+    function(spawn)
+        if Config.EnablePvP then
+            SetCanAttackFriendly(PlayerPedId(), true, false)
+            NetworkSetFriendlyFireOption(true)
+        end
+    end
+)
+
+--Show a player's own ID
+Citizen.CreateThread(
+    function()
+        while true do
+            SetTextFont(4)
+            SetTextDropShadow(0, 0, 0, 0, 255)
+            SetTextEdge(0, 0, 0, 0, 255)
+            SetTextScale(0.50, 0.50)
+            SetTextColour(185, 185, 185, 255)
+            SetTextDropShadow()
+            SetTextOutline()
+            SetTextEntry("STRING")
+            AddTextComponentString("~y~ID: ~s~" .. GetPlayerServerId(PlayerId()))
+            DrawText(0.175, 0.95)
+
+            Wait(10)
+        end
+    end
+)
---
+++ pnp_utility/config.lua
@@ -0,0 +1,3 @@
+Config = {}
+
+Config.EnablePvP = true
